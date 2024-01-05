20817eaef518d806a33f727c2e117776ec1d1dad
johnlawliet <t.shiladitya@gmail.com>
2024-01-05 23:05:49 +0530

    Added tests

--- app/main.py
+++ app/main.py
@@ -1,18 +1,54 @@
-import pathlib
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import pathlib, io, uuid
+from functools import lru_cache
+from fastapi import (
+    Depends, FastAPI, Request, File, UploadFile
+)
+from typing_extensions import Annotated
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic_settings import BaseSettings
+
+# This is needed to enable debug mmode. Debug mode is picked up based on value in .env
+# note that this requires python-dotenv
+class Settings(BaseSettings):
+    debug: bool = False
+    echo_active: bool = False
+
+    class Config:
+        env_file = ".env"
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+settings = get_settings()
+DEBUG=settings.debug
 
 BASE_DIR = pathlib.Path(__file__).parent
-print(BASE_DIR / "templates")
+UPLOAD_DIR = BASE_DIR / "uploads"
 
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 @app.get("/", response_class=HTMLResponse)
-def home_view(request: Request):
+def home_view(request: Request, settings: Annotated[Settings, Depends(get_settings)]):
     return templates.TemplateResponse("home.html", {"request":request, "abc":123})
 
 @app.post("/")
 def home_view(request: Request):
     return {"hello":"world"}
+
+
+# for uploading anything always use async
+@app.post("/img-echo", response_class=FileResponse)
+async def img_echo_view(file:UploadFile = File(...), settings:Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise Exception(detail="Invalid endpoint", status_code=400)
+    UPLOAD_DIR.mkdir(exist_ok=True)
+    bytes_str = io.BytesIO(await file.read())
+    fname = pathlib.Path(file.filename)
+    fext = fname.suffix                             # .jpg, .txt
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fext}"     # uuid1 comes with a timestamp
+    with open(str(dest), 'wb') as out:
+        out.write(bytes_str.read())
+    return file
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,7 +1,12 @@
-from app.main import app
+from app.main import app, BASE_DIR
+import pathlib
+import shutil
+import time
 from fastapi.testclient import TestClient
+from PIL import Image, ImageChops
 
 client = TestClient(app)
+
 
 def test_get_home():
     response = client.get("/")
@@ -14,3 +19,12 @@ def test_post_home():
     assert response.status_code == 200
     assert "application/json" in response.headers['content-type']
     assert response.json() == {"hello":"world"}
+
+
+# glob is used to list files within a file directory path based on special character given
+# def test_echo_home():
+#     img_saved_path = BASE_DIR / "images"
+#     for path in img_saved_path.glob("*"):
+#         print("############### -> ",path)
+#         response = client.post("/img-echo/", files={"file": open(path, 'rb')})
+#         assert True
